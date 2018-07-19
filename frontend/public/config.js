096031bb16eafa6227c6da44b9f86d3cad25551c
Chenyang Ji <chenyang.ji@yale.edu>
2018-07-19 12:34:35 -0700

    update frontend code

---
+++ frontend/public/config.js
@@ -0,0 +1,17 @@
+// svg config
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let svg_width = 1000;
+let svg_height = 1000;
+let scaleFactor = 3;
+
+// game config
+let mePosX = windowWidth / 2;
+let mePosY = windowHeight / 2;
+let maxHp = 50;
+let team_color_0 = "#3182bd";
+let team_color_1 = "#e6550d";
+let tank_size = 5;
+let bullet_size = tank_size / 2;
+let hpbarWidth = tank_size * 3;
+let hpbarHeight = tank_size / 4;
